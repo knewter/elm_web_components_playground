82a318ff83725dd1b98b2354b26e62c0af897836
Josh Adams <josh.rubyist@gmail.com>
2016-12-13 19:17:10 -0600

    Use vulcanize-loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const paths = require('../config/paths');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
 
   devtool: 'eval',
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-require('./main.css');
+// Load the webcomponentsjs polyfill
+require('script!../bower_components/webcomponentsjs/webcomponents.js')
 
-var Elm = require('./Main.elm');
+window.Polymer = {
+  dom: 'shadow',
+  lazyRegister: true,
+  useNativeCSSProperties: true,
+}
 
-var root = document.getElementById('root');
+// Require your main webcomponent file (that can be just a file filled with html imports, custom styles or whatever)
+require('vulcanize?es6=false!./imports.html')
 
-Elm.Main.embed(root);
+// Require our styles
+require('./main.css')
+
+window.addEventListener('WebComponentsReady', () => {
+  let Elm = require('./Main.elm')
+  let root = document.getElementById('root')
+  let app = Elm.Main.embed(root)
+})
