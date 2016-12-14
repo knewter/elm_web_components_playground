0386c2f340bf8f5d1ca84e052fff814e06d52ee0
Josh Adams <josh.rubyist@gmail.com>
2016-12-14 00:00:13 -0600

    Switch to the branch we were on after publishing

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+current_branch=`git rev-parse --abbrev-ref HEAD`
+
 git checkout -b gh-pages
 ./build.sh
 mv dist /tmp/elm-web-components-playground-dist
@@ -10,5 +12,5 @@ rm -fr /tmp/elm-web-components-playground-dist
 git add -f .
 git commit -m"Update with latest code."
 git push -f origin gh-pages
-git checkout master
+git checkout $current_branch
 git branch -D gh-pages
